dd9f131ce9df96be4881464efb5f7e1dd0d4ca95
jscott-thompson <j.scott.thompson.12@gmail.com>
2021-02-15 11:36:21 -0500

    Fix transformation to scaled design matrix. Adjusted input range for target velocity.

--- rad_create_lhs_run_matrix.m
+++ rad_create_lhs_run_matrix.m
@@ -1,8 +1,8 @@
 % RAD_CREATE_LHS_RUN_MATRIX Creates a run matrix using LHS
 %
 
-input_range = [50000  100000;  % True target range (m)
-                  50     300;  % True target radial velocity (m/s)
+input_range = [ 5000  100000;  % True target range (m)
+                -300     300;  % True target radial velocity (m/s)
                   50     300;  % Radar platform velocity (m/s)
                 1e-5    2e-5;  % PRI (s)
               2.5e-2 3.75e-2]; % Wavelength (m)
@@ -11,8 +11,8 @@ n_design_iterations = 10;
 X = lhsdesign(n_design_points,size(input_range,1),'criterion','maximin','Iterations',n_design_iterations);
 % X = lhsdesign(1000,5);
 
-scale_factor = (input_range(:,2)-input_range(:,1)) + input_range(:,1);
-scaled_X = X.*scale_factor';
+scale_factor = (input_range(:,2)-input_range(:,1));
+scaled_X = X.*scale_factor' + input_range(:,1)';
 
 show_matrix_plot = true;
 if show_matrix_plot
