4f388af8d6c71626fc6c2d46154b96c928ed246a
jscott-thompson <j.scott.thompson.12@gmail.com>
2021-03-16 21:33:59 -0400

    Added more complete checks on function results using stored reference matrices

--- getCFARLevelTest.m
+++ getCFARLevelTest.m
@@ -14,7 +14,9 @@ num_half_guard_cells = 1;
 required_snr = 6;
 detection_candidates = range_doppler_map > thresh*required_snr;
 
+
 assert(find(detection_candidates) == 45);
 
-
-assert((thresh(3,3) - 1.1793627701249 < 1e-15) && (thresh(5,5) - 0.710179428031297 < 1e-15));
+load getCFARLevelTest_references.mat
+assert(all(all(thresh == thresh_reference)));
+assert(all(all(num_cells == num_cells_reference)));
