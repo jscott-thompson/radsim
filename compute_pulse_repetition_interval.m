c9bc1841fc127f7ae04577bf14013bfde2777c82
jscott-thompson <j.scott.thompson.12@gmail.com>
2021-04-14 20:39:28 -0400

    Added compute_pulse_repetition_interval

---
+++ compute_pulse_repetition_interval.m
@@ -0,0 +1,6 @@
+function result = compute_pulse_repetition_interval(pulse_repetition_frequency)
+%COMPUTE_PULSE_REPETITION_INTERVAL Given the pulse repetition frequency
+% The result is the inverse of the pulse repetition frequency
+    result = 1/pulse_repetition_frequency;
+end
+
