9521032709bbb9b1679eb25fd8ca8547503634a1
jscott-thompson <j.scott.thompson.12@gmail.com>
2021-04-19 21:40:27 -0400

    Match revisitedRadar_NoClutter's method of converting peak SNR from dB to linear

--- add_receiver_noise.m
+++ add_receiver_noise.m
@@ -1,7 +1,7 @@
 function result = add_receiver_noise(normalized_doppler_processed_signal)
 %ADD_RECEIVER_NOISE Adds normally distributed noise, hard coded magnitude
     peak_snr_dB = 10; % TODO: Refactor into input parameter or calculate from some radar system description
-    peak_snr_linear = db2mag(peak_snr_dB);
+    peak_snr_linear = 10^(0.2*peak_snr_dB); % TODO: Understand why the hard-coded 0.2, and refactor
     noise = sqrt(1/peak_snr_linear)*...
         (randn(size(normalized_doppler_processed_signal))+...
          1j*randn(size(normalized_doppler_processed_signal)));
