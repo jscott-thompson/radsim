65594e404ae6c569bb6766229b840d5ec95c706a
jscott-thompson <j.scott.thompson.12@gmail.com>
2021-04-19 20:16:26 -0400

    Corrected spelling of 'unambiguous'

--- compute_unambiguous_range.m
+++ compute_unambiguous_range.m
@@ -1,4 +1,4 @@
-function result = compute_unambigious_range(prf)
+function result = compute_unambiguous_range(prf)
 %COMPUTE_UNAMBIGUOUS_RANGE Given the pulse repetition frequency
     result = 0.5*get_c()*compute_pulse_repetition_interval(prf);
 end
