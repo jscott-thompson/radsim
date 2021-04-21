85b035afef7da300a3ac4dc4c471607d235d7d8d
jscott-thompson <j.scott.thompson.12@gmail.com>
2021-04-20 22:16:12 -0400

    Corrected order of input arguments

--- add_lfm_baseband_modulation.m
+++ add_lfm_baseband_modulation.m
@@ -1,4 +1,4 @@
-function result = add_lfm_baseband_modulation(signal_envelope,bandwidth,pulse_width,starting_phase,chirp_direction)
+function result = add_lfm_baseband_modulation(signal_envelope,pulse_width,bandwidth,starting_phase,chirp_direction)
 %ADD_LFM_BASEBAND_MODULATION Superimpose a chirp over the signal
     result.t = signal_envelope.t;
     result.voltage = signal_envelope.voltage.*...
