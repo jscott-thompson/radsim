cb795ddcc6dcfb2d91860f9cf8e4161a6eaab670
jscott-thompson <j.scott.thompson.12@gmail.com>
2021-04-26 22:56:15 -0400

    Moved creation of radar signal up a level so the signal is available later in CFAR detector processing

--- rad_create_range_doppler_map.m
+++ rad_create_range_doppler_map.m
@@ -1,9 +1,8 @@
-function result = rad_create_range_doppler_map(waveform,radar_platform,target_platform)
+function result = rad_create_range_doppler_map(radar_signal,radar_platform,target_platform)
 %RAD_CREATE_RANGE_DOPPLER_MAP Create a range-Doppler map
 %   
 %
     rng(0);
-    radar_signal = create_signal(waveform);
     target_response = compute_target_response(radar_signal,radar_platform,target_platform);
     matched_filter_signal = compute_matched_filter_signal(radar_signal,target_response);
     doppler_processed_signal = compute_doppler_processed_signal(radar_signal,matched_filter_signal);
